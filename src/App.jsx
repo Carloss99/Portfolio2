4892c1c9443e05c14a317c1b808d3bd27bd39d20
carlos6199 <carlos_fig14@hotmail.com>
2024-07-04 23:27:55 -0400

    added media query to background header

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,20 @@ import { Animator, ScrollContainer, ScrollPage,FadeIn,FadeOut,Fade,batch, Move, 
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Anton&family=Barlow:ital,wght@1,500&family=Cinzel+Decorative&family=Dancing+Script:wght@500&family=Josefin+Sans:ital,wght@1,300&family=Rubik+Iso&family=Shantell+Sans:wght@300&family=Special+Elite&family=VT323&display=swap');
+
+<link rel="stylesheet" href="/App.css"/>
+
+
 </style>
 
 function App() {
   const mainStyle = {
-    height : "100vh",
-    backgroundImage :"url('https://plus.unsplash.com/premium_photo-1678565879444-f87c8bd9f241?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-    backgroundSize : "cover",
-    opacity: '0.5'
+    // height : "100vh",
+    // backgroundImage :"url('https://plus.unsplash.com/premium_photo-1678565879444-f87c8bd9f241?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+    // backgroundSize : "cover",
+    // opacity: '0.5',
+    
+    
   }
   const font = {
     fontFamily: "Shantell Sans, cursive"
@@ -33,13 +39,18 @@ function App() {
         <ScrollContainer>
           <ScrollPage>
           <Animator animation={Fade(0,1)}>
-            <div class="bg-black">
+            <div class="bg-black" >
 
-              <div class="" style={mainStyle}></div>
+              <div class="sm:h-screen h-80" style={{
+                
+                backgroundImage :"url('https://plus.unsplash.com/premium_photo-1678565879444-f87c8bd9f241?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                backgroundSize :"cover",
+                opacity:'0.5'
+              }} id='headerbg'></div>
 
               <SideBar/>
 
-              <div style={font} class="text-zinc-300 text-3xl absolute top-0 right-0 mt-60 mr-60 w-96">My name is Carlos Figueroa. I am a Softare Engineer.</div>
+              <div style={font} class="text-zinc-300 text-sm sm:text-3xl absolute top-0 right-0 sm:mt-60 sm:mr-60 mt-20 mr-5 w-96" >My name is Carlos Figueroa. I am a Softare Engineer.</div>
 
             </div>
             
@@ -49,7 +60,7 @@ function App() {
 
 
             <ScrollPage>
-          <Animator animation={batch(MoveIn(-1000,0), MoveOut(3000, 0))}>
+          <Animator animation={batch(MoveIn(-1000,0), MoveOut(2000, 0))}>
             <Skills/>
           </Animator>
           
@@ -58,7 +69,7 @@ function App() {
            
           
         <ScrollPage>
-          <Animator animation={batch(MoveIn(3000,0), MoveOut(-1000, 0))}>
+          <Animator animation={batch(MoveIn(1000,0), MoveOut(-2000, 0))}>
             <Projects/>
           </Animator>
           
